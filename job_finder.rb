4f8a282502b474b7cc00338aa713dc97a3130489
Einar Montalvo <einar.mn2407@gmail.com>
2017-07-22 10:39:03 -0400

    PK's idea

--- job_finder.rb
+++ job_finder.rb
@@ -5,32 +5,37 @@ def list(field)
 	link = "https://miami.craigslist.org/search/#{field}"
 	response = HTTParty.get("#{link}")
 	dom = Nokogiri::HTML(response.body)
-
-# p dom.css('a.hdrlnk').class
-
-	job_titles = []
-	dom.css('a.hdrlnk').each_with_index do |e,i|
-	job_titles << (i+1).to_s + "- " + e.content + "\n" + "\t" + e["href"]
+	data = []
+	dom.css('a.hdrlnk').each do |i|
+		data << i.content
 	end
-	puts job_titles
+	return data
 end
 
-search = true
-while search == true
-	puts "Choose different field if you want to update the search:"
-	puts "Admin, Business, Healthcare, Software"
-	input = gets.chomp.downcase
-	case input
-	when "admin"
-		list("ofc")
-	when "business"
-		list("bus")
-	when "healthcare"
-		list("hea")
-	when "software"
-		list("sof")
-	else
-		puts "We currently unable to connect the Craigslist website"
-		search = false
-	end
-end
+puts list "edu"
+# 	job_titles = []
+# 	dom.css('a.hdrlnk').each_with_index do |e,i|
+# 	job_titles << (i+1).to_s + "- " + e.content + "\n" + "\t" + e["href"]
+# 	end
+# 	p job_titles
+# end
+
+# search = true
+# while search == true
+# 	puts "Choose different field if you want to update the search:"
+# 	puts "Admin, Business, Healthcare, Software"
+# 	input = gets.chomp.downcase
+# 	case input
+# 	when "admin"
+# 		list("ofc")
+# 	when "business"
+# 		list("bus")
+# 	when "healthcare"
+# 		list("hea")
+# 	when "software"
+# 		list("sof")
+# 	else
+# 		puts "We currently unable to connect the Craigslist website"
+# 		search = false
+# 	end
+# end
